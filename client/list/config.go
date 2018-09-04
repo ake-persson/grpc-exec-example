6d0b9d63e7ee75050d37adf986a33ad294aac048
Michael Persson <michael.ake.persson@gmail.com>
2018-09-04 15:30:35 -0500

    add list cmd

---
+++ client/list/config.go
@@ -0,0 +1,42 @@
+package list
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Config struct {
+	Token   string `toml:"token,omitempty"`
+	Ca      string `toml:"ca,omitempty"`
+	AsJson  bool
+	DefPort int
+	Targets []string
+}
+
+func newConfig() *Config {
+	return &Config{
+		Token:   "~/.grpc-exec-example.tkn",
+		Ca:      "../tls_setup/certs/ca.pem",
+		AsJson:  false,
+		DefPort: 8083,
+	}
+}
+
+func usage(fl *flag.FlagSet) func() {
+	return func() {
+		fmt.Printf("Usage: client [options]\n\nOptions:\n")
+		fl.PrintDefaults()
+	}
+}
+
+func (c *Config) setFlags() *flag.FlagSet {
+	fl := flag.NewFlagSet("", flag.ExitOnError)
+	fl.Usage = usage(fl)
+
+	fl.StringVar(&c.Token, "token", c.Token, "Token for service account.")
+	fl.StringVar(&c.Ca, "ca", c.Ca, "TLS CA certificate.")
+	fl.BoolVar(&c.AsJson, "json", c.AsJson, "Output as JSON.")
+	fl.IntVar(&c.DefPort, "def-port", c.DefPort, "Default port for info-server.")
+
+	return fl
+}
---
+++ client/list/list.go
@@ -0,0 +1,76 @@
+package list
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/mickep76/auth/jwt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/mickep76/grpc-exec-example/conf"
+	pb_info "github.com/mickep76/grpc-exec-example/info"
+	"github.com/mickep76/grpc-exec-example/tlscfg"
+)
+
+func list(c *Config, addr string, cfg *tls.Config, creds credentials.PerRPCCredentials) {
+	conn, err := grpc.Dial(addr,
+		grpc.WithTransportCredentials(credentials.NewTLS(cfg)),
+		grpc.WithPerRPCCredentials(creds))
+	if err != nil {
+		log.Printf("connect: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	clnt := pb_info.NewInfoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	s, err := clnt.ListSystems(ctx, &pb_info.ListRequest{})
+	if err != nil {
+		log.Printf("list: %v", err)
+		return
+	}
+
+	//	if c.AsJson {
+	b, _ := json.MarshalIndent(s, "", "  ")
+	fmt.Println(string(b))
+	//	} else {
+	//		fmt.Print(s.FmtStringColor(addr))
+	//	}
+}
+
+func Cmd(args []string) {
+	c := newConfig()
+	if err := conf.Load([]string{"/etc/client.toml", "~/.client.toml"}, c); err != nil {
+		log.Fatalf("config: %v", err)
+	}
+	fl := c.setFlags()
+	conf.ParseFlags(fl, args, c)
+
+	if len(fl.Args()) < 1 {
+		usage(fl)
+		os.Exit(0)
+	}
+	c.Targets = strings.Split(fl.Args()[0], ",")
+
+	tlsCfg, err := tlscfg.NewConfig(c.Ca, "", "", "", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	token, err := jwt.LoadSignedToken(c.Token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	list(c, c.Targets[0], tlsCfg, token)
+}
