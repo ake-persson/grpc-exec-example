ba3b41b290be39547407b596f0226b43cacb4dd1
Michael Persson <michael.ake.persson@gmail.com>
2019-06-25 11:52:11 +0200

    add Makefile

--- ldap-server/server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -t -p 389:389 -p 636:636 \
-	-v $PWD/../tls_setup/certs:/etc/ssl/certs \
-	-e TLS_CA="/etc/ssl/certs/ca.pem" \
-	-e TLS_CERT="/etc/ssl/certs/ldap.pem" \
-	-e TLS_KEY="/etc/ssl/certs/ldap.key" \
-	ldap-server
