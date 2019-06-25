0d425a032344fe92838795e4c681b0552a8c2280
Michael Persson <michael.ake.persson@gmail.com>
2019-06-25 11:09:38 +0200

    initial ldap server config

---
+++ ldap-server/init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sed -i "s!%SUFFIX%!$SUFFIX!g" /etc/openldap/slapd.conf
+sed -i "s!%ADMIN%!$ADMIN!g" /etc/openldap/slapd.conf
+sed -i "s!%PASS%!$PASS!g" /etc/openldap/slapd.conf
+
+sed -i "s!%TLS_CA%!$TLS_CA!g" /etc/openldap/slapd.conf
+sed -i "s!%TLS_KEY%!$TLS_KEY!g" /etc/openldap/slapd.conf
+sed -i "s!%TLS_CERT%!$TLS_CERT!g" /etc/openldap/slapd.conf
+sed -i "s!%TLS_VERIFY%!$TLS_VERIFY!g" /etc/openldap/slapd.conf
+
+sed -i "s!%SUFFIX%!$SUFFIX!g" /etc/openldap/ldif/*.ldif
+sed -i "s!%ORG%!$ORG!g" /etc/openldap/ldif/*.ldif
+
+for f in /etc/openldap/ldif/*.ldif; do
+	echo "Adding: $f"
+	slapadd -l $f
+done
+
+#exec /usr/sbin/slapd -h "ldap:/// ldaps:///" "$@"
+/usr/sbin/slapd -h "ldap:/// ldaps:///" "$@"
+
+while [ 1 ]; do
+	sleep 1
+done
--- ldap-server/server.sh
+++ ldap-server/server.sh
@@ -2,10 +2,7 @@
 
 docker run -t -p 389:389 -p 636:636 \
 	-v $PWD/../tls_setup/certs:/etc/ssl/certs \
-	-e ORGANISATION_NAME="Example" \
-	-e SUFFIX="dc=example,dc=com" \
-	-e TLS_VERIFY_CLIENT="allow" \
-	-e CA_FILE="/etc/ssl/certs/ca.pem" \
-	-e KEY_FILE="/etc/ssl/certs/ldap.key" \
-	-e CERT_FILE="/etc/ssl/certs/ldap.pem" \
+	-e TLS_CA="/etc/ssl/certs/ca.pem" \
+	-e TLS_CERT="/etc/ssl/certs/ldap.pem" \
+	-e TLS_KEY="/etc/ssl/certs/ldap.key" \
 	ldap-server
